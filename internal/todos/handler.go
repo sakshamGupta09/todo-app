810e73262a24bec332a3c10e8797544467682514
Saksham Gupta <saksham.09.gupta@gmail.com>
2026-06-26 11:58:59 +0530

    Hanlders implementation

--- internal/todos/handler.go
+++ internal/todos/handler.go
@@ -31,3 +31,67 @@ func (h *Handler) CreateTodo(w http.ResponseWriter, r *http.Request) {
 	}
 	utils.WriteJSON(w, http.StatusCreated, res)
 }
+
+func (h *Handler) GetTodos(w http.ResponseWriter, r *http.Request) {
+	var req GetTodosRequest
+	userId, err := utils.ToInt(r, "id")
+
+	if err != nil {
+		http.Error(w, ID_MISSING, http.StatusBadRequest)
+		return
+	}
+	if err := h.app.Decoder.Decode(&req, r.URL.Query()); err != nil {
+		http.Error(w, VALIDATION, http.StatusBadRequest)
+		return
+	}
+	if req.PageNumber == 0 {
+		req.PageNumber = 1
+	}
+	if req.PageSize == 0 {
+		req.PageSize = 10
+	}
+	res, error := h.service.GetTodos(r.Context(), userId, req)
+
+	if error != nil {
+		e := utils.CreateAPIError(error)
+		utils.WriteJSON(w, e.StatusCode, e)
+		return
+	}
+	utils.WriteJSON(w, http.StatusOK, res)
+}
+
+func (h *Handler) GetTodoById(w http.ResponseWriter, r *http.Request) {
+	todoId, err := utils.ToInt(r, "id")
+
+	if err != nil {
+		http.Error(w, ID_MISSING, http.StatusBadRequest)
+		return
+	}
+
+	res, error := h.service.GetTodoDetails(r.Context(), todoId)
+
+	if error != nil {
+		e := utils.CreateAPIError(error)
+		utils.WriteJSON(w, e.StatusCode, e)
+		return
+	}
+	utils.WriteJSON(w, http.StatusOK, res)
+}
+
+func (h *Handler) DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	todoId, err := utils.ToInt(r, "id")
+
+	if err != nil {
+		http.Error(w, ID_MISSING, http.StatusBadRequest)
+		return
+	}
+
+	error := h.service.DeleteTodo(r.Context(), todoId)
+
+	if error != nil {
+		e := utils.CreateAPIError(error)
+		utils.WriteJSON(w, e.StatusCode, e)
+		return
+	}
+	utils.WriteJSON(w, http.StatusOK, nil)
+}
--- internal/todos/messages.go
+++ internal/todos/messages.go
@@ -1,9 +1,9 @@
 package todos
 
 const (
-	NOT_FOUND       = "Resource not found"
-	ALREADY_EXISTS  = "RESOURCE_ALREADY_EXISTS"
-	INTERNAL_ERROR  = "Internal Server Error"
-	VALIDATION      = "VALIDATION_ERROR"
-	USER_ID_MISSING = "User Id is required"
+	NOT_FOUND      = "Resource not found"
+	ALREADY_EXISTS = "RESOURCE_ALREADY_EXISTS"
+	INTERNAL_ERROR = "Internal Server Error"
+	VALIDATION     = "VALIDATION_ERROR"
+	ID_MISSING     = "Resource Id is missing"
 )
